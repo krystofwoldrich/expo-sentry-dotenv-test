8dccb38f5c676d6c8f8296058c1460ca5e4de79b
Krystof Woldrich <krystofwoldrich@gmail.com>
2024-12-09 17:53:27 +0100

    Add Sentry

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://dd0c90203292438e91e2119af975c53b@o1357066.ingest.us.sentry.io/4505447416659968',
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // enableSpotlight: __DEV__,
+});
 
 export default function App() {
   return (
